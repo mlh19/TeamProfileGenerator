62233db828810e1b64136b981a0f9a7b631d797d
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-10-22 22:09:13 -0500

    add manager test and class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,42 @@
+const { it, expect, describe } = require("@jest/globals")
+const Employee = require("../lib/Employee")
+
+describe("Employee",() => {
+    describe("returns an object", () => {
+        it("should return an object", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const employee = new Employee(name, id, email)
+            expect(typeof(employee)).toEqual("object")
+        })
+    }) 
+    describe("Name", () => {
+        it("should return a name", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const employee = new Employee(name, id, email)
+            expect(employee.name).toEqual(name)
+        })
+
+    })
+    describe("Id", () => {
+        it("should return an id", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const employee = new Employee(name, id, email)
+            expect(employee.id).toEqual(id)
+        })
+    })
+    describe("Email", () => {
+        it("should return an email", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const employee = new Employee(name, id, email)
+            expect(employee.email).toEqual(email)
+        })
+    })
+}) 
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,56 @@
+const { it, expect, describe } = require("@jest/globals")
+const Manager = require("../lib/Manager")
+
+describe("Manager",() => {
+    describe("returns an object", () => {
+        it("should return an object", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const officeNumber = "830-998-6717"
+            const manager = new Manager(name, id, email, officeNumber)
+            expect(typeof(manager)).toEqual("object")
+        })
+    }) 
+    describe("Name", () => {
+        it("should return a name", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const officeNumber = "830-998-6717"
+            const manager = new Manager(name, id, email, officeNumber)
+            expect(manager.name).toEqual(name)
+        })
+
+    })
+    describe("Id", () => {
+        it("should return an id", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const officeNumber = "830-998-6717"
+            const manager = new Manager(name, id, email, officeNumber)
+            expect(manager.id).toEqual(id)
+        })
+    })
+    describe("Email", () => {
+        it("should return an email", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const officeNumber = "830-998-6717"
+            const manager = new Manager(name, id, email, officeNumber)
+            expect(manager.email).toEqual(email)
+        })
+    }) 
+    describe("officeNumber", () => {
+        it("should return a number", () => {
+            const name = "Bob"
+            const id = "10"
+            const email = "test@test.com"
+            const officeNumber = "830-998-6717"
+            const manager = new Manager(name, id, email, officeNumber)
+            expect(manager.officeNumber).toEqual(officeNumber)
+        })
+    })
+}) 
