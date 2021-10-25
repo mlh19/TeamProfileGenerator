1c01bac0034f351dd27b91d3252db7cf202e6288
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-10-24 20:47:42 -0500

    finish question prompts, menu and add functions for all team members that will generate the html.

--- index.js
+++ index.js
@@ -6,77 +6,173 @@ const Intern = require("./lib/Intern")
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+// Team Members array of objects
+const teamMembers = [];
+// Reference to the generateHtml.js file
+const generate = require("./dist/generateHtml.js");
 
-
-
-// TODO: Create an array of questions for user input
-const htmlQuestions = [
-  {
-    type: "input",
-    message: "What is the title of your application?",
-    name: "title", 
-  },
-  {
-    type: "input",
-    message: "Provide a description of your application?",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "How can others you install your application?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "How do you use your application?",
-    name: "usage",
-  },
-  {
-    type: "checkbox",
-    message: "What License did you use for this repository?",
-    choices: 
-    [ "MIT",
-      "GNU General Public License 2.0",
-      "Apache License 2.0",
-      "GNU General Public License 3.0"],
-    name: "license", 
-  },
-  {
-    type: "input",
-    message: "How can others contribute to this project?",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "How can others update the tests for your project?",
-    name: "tests"
-  },
-  {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "github",
-  },
-  {
-    type: "input",
-    message: "What is your email address where users can contact you?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "Is there any other information you wish to provide?",
-    name: "other",
-  }
+// Questions to fill in for the Manager constructor
+const managerQuestions = [
+	{
+		type: 'input',
+		message: "What is the Manager's name?",
+		name: 'name',
+	},
+	{
+		type: 'input',
+		message: "What is the Manager's ID number?",
+		name: 'managerId',
+	},
+	{
+        type: 'input',
+		message: "What is the Manager's e-mail?",
+		name: 'email',
+	},
+	{
+		type: 'input',
+		message: "What is the Manager's office number?",
+		name: 'officeNumber',
+	}
 ];
 
-// TODO: Create a function to write HTML file
-function promptUser() {
-  inquirer
-    .prompt(htmlQuestions)
-    .then((response) =>
-      fs.writeFileSync("index.html", generate.generateHtml(response))
-    )
+
+// Questions to fill in for the Engineer constructor
+const engineerQuestions = [
+	{
+		type: 'input',
+		message: "What is the Engineer's name?",
+		name: 'name',
+	},
+	{
+		type: 'input',
+		message: "What is the Engineer's ID number?",
+		name: 'engineerId',
+	},
+	{
+        type: 'input',
+		message: "What is the Engineer's e-mail?",
+		name: 'email',
+	},
+	{
+		type: 'input',
+		message: "What is the Engineer's GitHub Username?",
+		name: 'githubUsername',
+	},
+];
+// Questions to fill in for the Intern constructor
+const internQuestions = [
+	{
+		type: 'input',
+		message: "What is the Intern's name?",
+		name: 'name',
+	},
+	{
+		type: 'input',
+		message: "What is the Intern's ID number?",
+		name: 'managerId',
+	},
+	{
+        type: 'input',
+		message: "What is the Intern's e-mail?",
+		name: 'email',
+	},
+	{
+		type: 'input',
+		message: "What school is the Intern attending?",
+		name: 'school',
+	},
+];
+// Function to prompt user questions about the Manager
+function promptManagerQuestions() {
+    inquirer
+    .prompt(managerQuestions)
+    .then((response) => {
+    console.log(response);
+    // Gets the value for each key and assigns them individually in those variables.
+    const {name, managerId, email, officeNumber} = response;
+    // Create a new Manager instance and add it to the list.
+    const manager = new Manager(name, managerId, email, officeNumber);
+    teamMembers.push(manager);
+    // Call the next questions.
+    promptMenu();
+
+    //   fs.writeFileSync("index.html", generate.generateHtml(response))
+    })
     .then(() => console.log("Successfully wrote to index.html"))
     .catch((err) => console.error(err));
 }
 
-promptUser();
+
+function promptMenu() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'menuOption',
+            message: "What would you like to do next",
+            choices: [
+                'Add an Engineer',
+                'Add an Intern',
+                'Finish Building My Team'
+            ]
+        }
+    ])
+    .then((response) => {
+        if(response.menuOption === 'Add an Engineer') {
+            promptEngineerQuestions();
+        } else if (response.menuOption === 'Add an Intern') {
+            promptInternQuestions();
+        } else if (response.menuOption === 'Finish Building My Team') {
+            promptTeamQuestions();
+        } else {
+            console.log("No option exists named " + response.menuOption);
+        }
+    })
+}
+// Function to prompt user questions about the Engineer
+function promptEngineerQuestions() {
+    inquirer
+    .prompt(engineerQuestions)
+    .then((response) => {
+    console.log(response);
+    // Gets the value for each key and assigns them individually in those variables.
+    const {name, engineerId, email, githubUsername} = response;
+    // Create a new Manager instance and add it to the list.
+    const engineer = new Engineer(name, engineerId, email, githubUsername);
+    teamMembers.push(engineer);
+    // Call the next questions.
+    promptMenu();
+    })
+}
+// Function to prompt user questions about the Intern
+function promptInternQuestions() {
+    inquirer
+    .prompt(internQuestions)
+    .then((response) => {
+    console.log(response);
+    // Gets the value for each key and assigns them individually in those variables.
+    const {name, internId, email, school} = response;
+    // Create a new Manager instance and add it to the list.
+    const intern = new Intern(name, internId, email, school);
+    teamMembers.push(intern);
+    promptMenu();
+
+
+ fs.writeFileSync("index.html", generate.generateHtml(response))
+    })
+    .then(() => console.log("Successfully wrote to index.html"))
+    .catch((err) => console.error(err));
+}
+
+
+
+
+
+
+
+
+
+
+
+// Entry to the Program
+promptManagerQuestions();
