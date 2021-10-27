f50e9231f4bd21383327c2decfbc53e7d4907838
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-10-27 10:44:36 -0500

    make styling change to the cards

--- dist/generateHtml.js
+++ dist/generateHtml.js
@@ -4,53 +4,54 @@ function generateHtml(data) {
         const managerData = data.filter((employee) => employee.getRole() === "Manager")
         // console.log(managerData)
         return `
+    <div class="card m-2" style="min-width: 20rem;">
         <div class="card-body">
-        <h4 class="card-title">${managerData[0].name}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+            <h4 class="card-title">Name: ${managerData[0].name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+        </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">${managerData[0].id}</li>
-            <li class="list-group-item">${managerData[0].email}</li>
-            <li class="list-group-item">${managerData[0].officeNumber}</li>
+            <li class="list-group-item">ID: ${managerData[0].id}</li>
+            <li class="list-group-item">Email: ${managerData[0].email}</li>
+            <li class="list-group-item">Office Number: ${managerData[0].officeNumber}</li>
         </ul>
         </div>
         `
     }
-     const engineer = () => {
-         const engineerData = data.filter((employee) => employee.getRole() === "Engineer")
+    const engineer = () => {
+        const engineerData = data.filter((employee) => employee.getRole() === "Engineer")
             return engineerData.map((engineer) => `
-         <div class="card-body">
-            <h4 class="card-title">${engineer.name}</h5>
+    <div class="card m-2" style="min-width: 20rem;">
+        <div class="card-body">
+            <h4 class="card-title">Name: ${engineer.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
+        </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">${engineer.id}</li>
-                <li class="list-group-item">${engineer.email}</li>
-                <li class="list-group-item">${engineer.githubUsername}</li>
-           
-         </ul>
-     </div>
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: ${engineer.email}</li>
+            <li class="list-group-item">GitHub: ${engineer.githubUsername}</li>
+        </ul>
+        </div>
         `
         ).join("")
-     }
+    }
     
-        const intern = () => {
-            const internData = data.filter((employee) => employee.getRole() === "Intern")
+    const intern = () => {
+        const internData = data.filter((employee) => employee.getRole() === "Intern")
             return internData.map((intern) => `
-
-            
-            <div class="card-body">
-            <h4 class="card-title">${intern.name}</h5>
+    <div class="card m-2" style="min-width: 20rem;">
+        <div class="card-body">
+            <h4 class="card-title">Name: ${intern.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Intern</h6>
-           <ul class="list-group list-group-flush">
-                <li class="list-group-item">${intern.id}</li>
-               <li class="list-group-item">${intern.email}</li>
-               <li class="list-group-item">${intern.school}</li>
-              
-            </ul>
         </div>
-           `).join("")}
-           
-  return `
-  
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email: ${intern.email}</li>
+            <li class="list-group-item">School: ${intern.school}</li>
+        </ul>
+        </div>
+        `).join("")}
+            return `
+</div>
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -60,48 +61,25 @@ function generateHtml(data) {
     <meta name="Description" content="Enter your description here"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Team Profile Generator</title>
-    </head>
-    <body>
-        
-
-<nav class="navbar navbar-light bg-light">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title id="title">Team Profile Generator</title>
+    </head> 
+<body>
+<nav class="navbar navbar-light bg-light justify-content-center">
     <span class="navbar-brand mb-0 h1">My Team</span>
 </nav>
-    <div class="card" style="width: 18rem;">
+<div id="teamMember-Cards" class="container d-flex flex-wrap justify-content-center align-items-center">
         ${manager()}
-    </div>
-    <div class="card" style="width: 18rem;">
         ${engineer()}
-    </div>
-    <div class="card" style="width: 18rem;">
         ${intern()}
-    </div> 
-    
-
-        
-    
-    
-
-
-
-
-
-
-
-
+</div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
 </body>
 </html>     
-    
-    `
+`
 }  
-    
-    
-
 
 module.exports = generateHtml;
